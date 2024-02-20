0359277ab936bc159211f8ab51b496e586984f73
KevinTran99 <kevintrann99@gmail.com>
2024-02-20 19:19:34 +0100

    Added script to view balance on eth accounts

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+const accountInput = document.querySelector('#accountNumber');
+const checkBalanceButton = document.querySelector('#checkBalance');
+const displayBalance = document.querySelector('#balance');
+
 const rpc = new Web3(
   'https://eth-sepolia.g.alchemy.com/v2/kdwWXZC7BHd7LneRBqLTcpIcl6HhHJHD'
 );
+
+let account;
 
 function initApp() {
   console.log(rpc);
 }
 
+async function checkBalance() {
+  account = accountInput.value;
+  const balance = await rpc.eth.getBalance(account);
+  displayBalance.innerHTML = rpc.utils.fromWei(balance, 'ether');
+}
+
 document.addEventListener('DOMContentLoaded', initApp);
+checkBalanceButton.addEventListener('click', checkBalance);
