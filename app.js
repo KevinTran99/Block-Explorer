7dbcdc6cf12f8561c54e27c9602bf6c0c40f4270
KevinTran99 <kevintrann99@gmail.com>
2024-02-22 18:05:41 +0100

    Added script to transfer eth

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const accountInput = document.querySelector('#accountNumber');
 const checkBalanceButton = document.querySelector('#checkBalance');
 const displayBalance = document.querySelector('#balance');
 
-const rpc = new Web3(
-  'https://eth-sepolia.g.alchemy.com/v2/kdwWXZC7BHd7LneRBqLTcpIcl6HhHJHD'
-);
+const toAccountInput = document.querySelector('#toAccountNumber');
+const valueInput = document.querySelector('#amount');
+const sendButton = document.querySelector('#sendTx');
+
+const rpc = new Web3('HTTP://127.0.0.1:7545');
 
 let account;
 
@@ -18,5 +20,21 @@ async function checkBalance() {
   displayBalance.innerHTML = rpc.utils.fromWei(balance, 'ether');
 }
 
+async function sendTransaction() {
+  const toAddress = toAccountInput.value;
+  const amount = valueInput.value;
+
+  try {
+    const trx = await rpc.eth.sendTransaction({
+      from: account,
+      to: toAddress,
+      value: rpc.utils.toWei(amount, 'ether'),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', initApp);
 checkBalanceButton.addEventListener('click', checkBalance);
+sendButton.addEventListener('click', sendTransaction);
